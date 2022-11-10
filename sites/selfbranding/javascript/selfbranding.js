f0caa893a9515c9e0748962ef88cfa4877b8cb53
cachilder <114614416+cachilder@users.noreply.github.com>
2022-11-10 15:31:42 -0500

    Add files via upload

--- sites/selfbranding/javascript/selfbranding.js
+++ sites/selfbranding/javascript/selfbranding.js
@@ -50,3 +50,35 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+var form = document.forms["contact"];
+form.addEventListener('submit',contact_submit,false);
+
+function contact_submit(e) {
+  // Stop Form From Submitting
+  e.preventDefault();
+
+  // Set Initial Variables
+  var target = e.target || e.srcElement;
+  var to = 'someone@example.com';
+  var uri = 'mailto:' + to;
+  var body = '';
+
+  // Set Form Values to Variables
+  var name = target.elements['name'].value;
+  var subject = target.elements['subject'].value;
+  var phone = target.elements['phone'].value;
+  var message = target.elements['message'].value;
+
+  // Build Body / Message with all Input Fields
+  body += message + "\r\n\r\n";
+  body += "Name: " + name + "\r\n";
+  body += "Phone Number: " + phone + "\r\n";
+
+  // Build final Mailto URI
+  uri += '?subject=' + encodeURIComponent(subject);
+  uri += '&body=' + encodeURIComponent(body);
+
+  // Open Mailto in New Window / Tab
+  window.open(uri,'_blank');
+}
